406add7aa0279c18c206ab73b184800988ebeb6a
Lingfeng555 <130897792+Lnfeng@users.noreply.github.com>
2024-03-01 17:37:13 +0100

    ErPepe  Changes to be committed:

---
+++ LoadDatasets.R
@@ -0,0 +1,10 @@
+Iq_Per_Country <- read.csv("RawDatasets/IQ_Per_Country.csv")
+Mental_Disorders_Per_Country_Per_Year <- read.csv("RawDatasets/Mental_Disorders_For_Coutries_Per_Year.csv")
+Pib_Per_Country <- read.csv("RawDatasets/PIB_Per_Country_Per_Year.csv")
+Suicide_Per_Country <- read.csv("RawDatasets/Suicide_Per_Country.csv")
+
+Happiness15_19 <- list(Happiness2015 = read.csv("RawDatasets/Hapiness/2015.csv"), 
+                       Happiness2016 = read.csv("RawDatasets/Hapiness/2016.csv"), 
+                       Happiness2017 = read.csv("RawDatasets/Hapiness/2017.csv"), 
+                       Happiness2018 = read.csv("RawDatasets/Hapiness/2018.csv"), 
+                       Happiness2019 = read.csv("RawDatasets/Hapiness/2019.csv")) 
