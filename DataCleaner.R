63018d6d8f5b8d9db3609be05b29772496bdb6c8
Martín Fernández de Retana <martin.f@opendeusto.es>
2024-03-30 20:02:54 +0100

    Happiness dataset cleaned

--- DataCleaner.R
+++ DataCleaner.R
@@ -3,7 +3,17 @@ if( length(ls()) == 0 ){
   source(paste(as.character(getwd()), "/LoadDatasets.R", sep = ""))
 }
 
+normalize <- function(data){
+  (data - min(data)) / (max(data) - min(data))
+}
+
 process_Hapiness <- function(rawHappiness){
+  # Remove unnecesary columns (Rank)
+  rawHappiness <- cbind(rawHappiness[,1:2], rawHappiness[,4:10])
+  
+  # Normalize data columns (All columns except country and year)
+  rawHappiness[,3:9] <- apply(rawHappiness[,3:9], 2, normalize)
+  
   rawHappiness
 }
 
@@ -23,12 +33,12 @@ process_Suicide <- function(rawSuicide){
   rawSuicide
 }
 
-Clean <- function(){
+clean_Datasets <- function(){
   happiness <- process_Hapiness(happiness)
   Iq_Per_Country <- process_Iq(Iq_Per_Country)
   Mental_Disorders <- process_Disorders(Mental_Disorders)
   Pib_Per_Country <- process_Pib(Pib_Per_Country)
-  Suicide_Per_Country <- process-Suicide(Suicide_Per_Country)
+  Suicide_Per_Country <- process_Suicide(Suicide_Per_Country)
 }
 
-Clean()
+clean_Datasets()
