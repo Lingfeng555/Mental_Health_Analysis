af47ab1817db482a0a6a444ab210a0006206ea0c
Lingfeng555 <lingfeng.chen@opendeusto.es>
2024-04-01 01:26:31 +0200

    Changes to be committed:

--- DataCleaner.R
+++ DataCleaner.R
@@ -9,7 +9,7 @@ Load_Libraries <- function(packages){
   lapply(packages, library, character.only=TRUE)
 }
 
-Load_Libraries(c("forecast", "dplyr"))
+Load_Libraries(c("forecast", "dplyr", "data.table"))
 
 normalize <- function(data) ((data - min(data)) / (max(data) - min(data)))
 
@@ -35,12 +35,6 @@ process_Disorders <- function(rawDisorders){
   rawDisorders
 }
 
-Interpolate_Na <- function (rawGDP){
-  na_missing <- which(is.na(rawGDP$Y2021))
-  print(na_missing)
-  rawGDP
-} 
-
 NA_Rows_Replace <- function (rawGDP){
   na_rows <- which(rowSums(is.na(rawGDP)) == (ncol(rawGDP)-1)) #The column of the country names does not count
   if (length(na_rows) > 0) {
@@ -49,8 +43,37 @@ NA_Rows_Replace <- function (rawGDP){
   rawGDP
 }
 
+Traspose_DataFrame <- function(dataframe){
+  t_dataframe <- t(dataframe)
+  t_dataframe <- as.data.frame(dataframe)
+  return(t_dataframe)
+}
+
+Build_Model <- function(vector){
+  temp <- data.frame(
+    year = 2013:2021,
+    GDP_Per_Capita = vector
+  )
+  temp <- temp[!is.na(temp$GDP_Per_Capita),]
+  lm(GDP_Per_Capita~year,temp)
+}
+
+Interpolate_Na <- function (rawGDP){
+  na_index <- which(is.na(rawGDP$Y2021))
+  na_missing <- rawGDP[na_index,] # All missing values has at least a NA at 2021
+  years <- 2013:2021
+  for(i in 1:nrow(na_missing)){
+    GDP_vector <- na_missing[i, 2:ncol(na_missing)] #Get the vector of GDB per capita
+    model <- as.numeric(GDP_vector) %>% Build_Model #Build a lineal regression of each country
+    missing_year <- years[is.na(as.numeric(GDP_vector))] #Get the missing years
+    rawGDP[na_index[i], missing_year - 2013 + 2] <- predict(model, data.frame(year = missing_year))  #Fill with approximations
+  }
+  rawGDP
+} 
+
 process_GDP <- function(rawGDP){
   rawGDP <- rawGDP %>% NA_Rows_Replace %>% Interpolate_Na
+  rawGDP[,2:ncol(rawGDP)] <- apply(rawGDP[,2:ncol(rawGDP)], 2, normalize)
   rawGDP 
 }
 
@@ -58,12 +81,8 @@ process_Suicide <- function(rawSuicide){
   rawSuicide 
 }
 
-clean_Datasets <- function(){
-  happiness <- process_Hapiness(happiness)
-  Iq_Per_Country <- process_Iq(Iq_Per_Country)
-  Mental_Disorders <- process_Disorders(Mental_Disorders)
-  GDP_Per_Capita <- process_GDP(GDP_Per_Capita)
-  Suicide_Per_Country <- process_Suicide(Suicide_Per_Country)
-}
-
-clean_Datasets()
+happiness <- process_Hapiness(happiness)
+Iq_Per_Country <- process_Iq(Iq_Per_Country)
+Mental_Disorders <- process_Disorders(Mental_Disorders)
+GDP_Per_Capita <- process_GDP(GDP_Per_Capita)
+Suicide_Per_Country <- process_Suicide(Suicide_Per_Country)
